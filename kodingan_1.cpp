16355260bcb2e88bceab4453fd1f0e7b84f22df1
xyerakovalskia <xyera123987@gmail.com>
2026-03-09 19:07:33 +0700

    bagian_2

--- kodingan_1.cpp
+++ kodingan_1.cpp
@@ -6,4 +6,11 @@ int main(){
 
     srand(time(0));
     x = rand() % 10;
+
+    if (x == 5){
+        x = x + 1;
+        cout << "nilai x sudah dikoreksi menjadi : " << x << endl;
+    }
+
+    cout << "Bilangan yang diberikan = " << x;
 }
